748022a991343eed51bfd6ee9c5af40b7342e84b
Bernardo A Mira García <bamira1@misena.edu.co>
2023-12-04 15:36:09 -0500

    se crea proyecto

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Entradas del problema
+        String nombreUsuario="Juan Jose Gallego Mesa";
+        short edadUsuario=34;
+        String telefonoUsuario="3225962363";
+        String documentoIdentidad="1017187878";
+        int cuotaEnero;
+        int cuotaFebrero;
+        int cuotaMarzo;
+        int cuotaAbril;
+        int cuotaMayo;
+        int cuotaJunio;
+        int cuotaJulio;
+        int cuotaAgosto;
+        int cuotaSeptiembre;
+        int cuotaOctubre;
+        int cuotaNoviembre;
+        String direccionUsuario="Cll 50# 80-20";
+        double costoInscripcion=150000.0;
+        float tasaInteresNatillera=0.1F;
+        double saldoFinalUsuario;
+
+        //PROCESO
+
+        //1. Asignar un valor especifico a una variable
+        //desde el TECLADO (Recibir datos)
+        Scanner leerDato = new Scanner(System.in);
+
+        System.out.println("Hola soy una IA, cual es tu nombre?");
+        //leerDato.nextLine();
+
+
+
+    }
+}
